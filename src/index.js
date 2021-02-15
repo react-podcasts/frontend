a12a90cd49e7d84540f7c7eb440d71b63216ab95
Ilya Agafonov <cubucul@gmail.com>
2021-02-15 15:27:22 +0500

    Добавляет Redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from './components/app';
 import './styles/index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>React Podcasts</h1>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
