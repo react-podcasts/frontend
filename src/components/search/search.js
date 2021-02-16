c18e821046e0093c1931c53f9b4b75fc0ae636aa
Ilya Agafonov <cubucul@gmail.com>
2021-02-16 15:21:28 +0500

    Стилизует поиск

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { findPodcasts } from '../../actions/search';
+import { ReactComponent as SearchIcon } from './search.svg';
+import { ReactComponent as SpinnerIcon } from './spinner.svg';
+import './search.css';
 
 const Search = () => {
+  const [showResults, setShowResults] = useState(false);
   const { term, loading, error, results } = useSelector(state => state.search);
   const dispatch = useDispatch();
 
@@ -21,32 +25,67 @@ const Search = () => {
   };
 
   return (
-    <div>
+    <div className="search">
       <form onSubmit={handleSubmit}>
-        <label htmlFor="search">Find podcasts</label>
-        <input
-          id="search"
-          type="text"
-          value={term}
-          onChange={handleChange}
-          placeholder="Find podcasts"
-        />
+        <div className="search__field">
+          <label className="visually-hidden" htmlFor="search">Find podcasts</label>
+          <input
+            className="search__input"
+            id="search"
+            type="text"
+            value={term}
+            onChange={handleChange}
+            onFocus={() => setShowResults(true)}
+            onBlur={() => setShowResults(false)}
+            placeholder="Find podcasts"
+          />
+          { !loading &&
+            <SearchIcon
+              className="search__icon-search"
+              width="18"
+              height="18"
+              aria-hidden="true"
+            />
+          }
+          { loading &&
+            <SpinnerIcon
+              className="search__icon-spinner"
+              width="18"
+              height="18"
+              aria-hidden="true"
+            />
+          }
+        </div>
       </form>
-      { loading && <p>Loading...</p> }
-      { error && <p>Podcasts not found</p> }
-      <ul>
-        {
-          results.map(({ id, title, author, coverUrl60 }) => {
-            return (
-              <li key={id}>
-                <img src={coverUrl60} alt={title}/>
-                <h4>{title}</h4>
-                <span>{author}</span>
-              </li>
-            );
-          })
-        }
-      </ul>
+      { showResults && term.length > 0 &&
+        <div className="search__menu">
+          { error &&
+            <div className="search__error">
+              <h3 className="search__error-title">No podcasts found</h3>
+              <p className="search__error-text">Try more general, or different, keywords</p>
+            </div>
+          }
+          { results.length > 0 &&
+            <ul className="search__list">
+              {
+                results.map(({ id, title, author, coverUrl60 }) => {
+                  return (
+                    <li key={id} className="search__item">
+                      <a className="search__link" href="/">
+                        <img className="search__cover" src={coverUrl60} alt={title}/>
+                        <div>
+                          <h4 className="search__title">{title}</h4>
+                          <p className="search__author">{author}</p>
+                        </div>
+                      </a>
+                    </li>
+                  );
+                })
+              }
+            </ul>
+          }
+        </div>
+      }
     </div>
   );
 };
