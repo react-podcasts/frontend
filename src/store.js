3f23c7403639dc96554759fa2f77b291d2451c12
Ilya Agafonov <cubucul@gmail.com>
2021-03-13 22:23:39 +0500

    Добавляет сохранение в localStorage

--- src/store.js
+++ src/store.js
@@ -2,6 +2,24 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const localStorageKey = 'react-podcasts';
+const persistedState = localStorage.getItem(localStorageKey);
+let preloadedState;
+
+if (persistedState) {
+  preloadedState = {
+    ...JSON.parse(persistedState)
+  };
+}
+
+const store = createStore(rootReducer, preloadedState, applyMiddleware(thunk));
+
+store.subscribe(() => {
+  const { subscriptions, listeningHistory } = store.getState();
+  localStorage.setItem(localStorageKey, JSON.stringify({
+    subscriptions,
+    listeningHistory
+  }));
+});
 
 export default store;
