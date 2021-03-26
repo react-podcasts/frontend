dd224e0da265af3b5079a9ae0e0931a6a9df1c40
Ilya Agafonov <cubucul@gmail.com>
2021-03-26 17:10:47 +0500

    Правит путь

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from './store';
-import App from './components/app';
+import App from './components/common/app';
 import './styles/index.css';
 import reportWebVitals from './reportWebVitals';
 
